cfdd21ecbd11252148582299c477ce31591d5853
alvinobarboza <32014216+alvinobarboza@users.noreply.github.com>
2017-11-15 18:00:19 -0200

    Testes com captura das pontuações dos jogos

---
+++ src/com/service/JogosService.java
@@ -0,0 +1,46 @@
+package com.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.dao.JogadoresDAO;
+import com.model.Jogadores;
+import com.servicecontroller.JogadoresController;
+import com.servicecontroller.Logar;
+
+
+
+@Path("/JogosTeste") 
+public class JogosService {
+	
+	String num;
+		
+	/*@GET 
+	@Path("/Jogadores") 
+	@Produces(MediaType.APPLICATION_JSON)
+	public Jogadores[] getJogadores() {
+	
+		return dtoList;
+		
+	}*/
+	
+	@GET
+	@Path("/derrota/{i}")
+	public void autenticar(@PathParam("i") String logar) {
+				
+		
+		System.out.println(logar);
+	
+	}
+}
--- src/com/servicecontroller/Logar.java
+++ src/com/servicecontroller/Logar.java
@@ -1,9 +1,21 @@
 package com.servicecontroller;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
 public class Logar {
 	private String login;
 	private String senha;
+	int teste;
 	
+	public int getTeste() {
+		return teste;
+	}
+	public void setTeste(int teste) {
+		this.teste = teste;
+	}
 	public String getLogin() {
 		return login;
 	}
